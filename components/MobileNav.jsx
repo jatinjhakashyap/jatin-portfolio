fcd8fe8a85e0b2a4f5be66c032d49064a8063361
Jatin <jatinjhakashyap@gmail.com>
2025-01-14 00:12:02 +0530

    Fixed Mobile Nav not getting closed after clicking on page

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -3,6 +3,7 @@
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
+import { useState } from "react";
 import { CiMenuFries } from "react-icons/ci";
 
 const links = [
@@ -30,9 +31,19 @@ const links = [
 
 const MobileNav = () => {
   const pathname = usePathname();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleLinkClick = () => {
+    // Close the Sheet after clicking a link
+    setIsOpen(false);
+  };
+
   return (
-    <Sheet>
-      <SheetTrigger className="flex justify-center items-center">
+    <Sheet open={isOpen} onOpenChange={setIsOpen}>
+      <SheetTrigger
+        className="flex justify-center items-center"
+        onClick={() => setIsOpen(true)}
+      >
         <CiMenuFries className="text-[32px] text-accent" />
       </SheetTrigger>
       <SheetContent className="flex flex-col">
@@ -55,6 +66,7 @@ const MobileNav = () => {
                   link.path === pathname &&
                   "text-accent border-b-2 border-accent"
                 } text-xl capitalize hover:text-accent transition-all`}
+                onClick={handleLinkClick}
               >
                 {link.name}
               </Link>
